3b27bd7e711c2104a98007c5b72985570993eacc
Stefan Ziegler <edi.gonzales@gmail.com>
2018-12-26 21:09:46 +0100

    script

--- scripts/grab_xml.groovy
+++ scripts/grab_xml.groovy
@@ -5,6 +5,8 @@ def baseUrl = "https://geoview.bl.ch/main/oereb/extract/reduced/"
 new File('bl/2765.csv').eachLine { line ->
     println line
     try {
+        def startDate = new Date().getTime()
+
         // get xml and pretty print it
         def xmlRequestUrl = baseUrl + "xml/geometry/" + line.trim() + "?WITHIMAGES"
         println xmlRequestUrl
@@ -15,6 +17,9 @@ new File('bl/2765.csv').eachLine { line ->
         def xmlFile = new File("../bl/" + line.trim() + ".xml")
         def xmlNodePrinter = new XmlNodePrinter(new PrintWriter(xmlFile))
         xmlNodePrinter.print(response)
+        def xmlDate = new Date().getTime()
+        println "xml (sec): " + (xmlDate - startDate) / 1000.0
+
 
         // get pdf
         def pdfRequestUrl = baseUrl + "pdf/" + line.trim()
@@ -23,7 +28,9 @@ new File('bl/2765.csv').eachLine { line ->
         def pdfFile = new File("../bl/" + line.trim() + ".pdf").newOutputStream()
         pdfFile << new URL(pdfRequestUrl).openStream()
         pdfFile.close()
-    } catch (java.io.FileNotFoundException e) {
+        def pdfDate = new Date().getTime()
+        println "pdf (sec): " + (pdfDate - xmlDate) / 1000.0
+    } catch (java.io.FileNotFoundException | Exception e) {
         println e.getMessage()
     }
 }
